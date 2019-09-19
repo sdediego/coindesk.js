ce521b29ea6f5819bdfbf16a2bcc1e936037f79d
Sergio de Diego <sergio.dediego@outlook.com>
2019-09-19 23:09:17 +0200

    Add index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Coindesk API client.
+ */
+
+const { CoindeskAPIClient, CoindeskAPIHttpRequest, CoindeskAPIHttpResponse } = require('./src/coindesk');
+
+module.exports = {
+    CoindeskAPIClient,
+    CoindeskAPIHttpRequest,
+    CoindeskAPIHttpResponse
+};
